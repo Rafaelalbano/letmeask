579a9a05781eb771b524b617287827bdddd44e9f
Rafael Albano <rafaelb.albano@gmail.com>
2021-06-22 00:56:22 -0300

    Adicionado rotas, autenticação e template criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import {Button} from './components/Button'
+//import { createContext, useState, useEffect } from 'react';
+
+//import {Button} from './components/Button';
+import { Home } from './pages/Home';
+import './styles/global.scss';
+import {NewRoom} from './pages/NewRoom';
+import {BrowserRouter, Route} from 'react-router-dom';
+//import firebase from 'firebase';
+//import { auth } from './services/firebase';
+
+import {AuthContextProvider} from './contexts/AuthContexts';
 
 function App() {
+
   return (
-    <div>
-      <Button text="Botão 1"/>
-      <Button />
-      <Button />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+    
   );
 }
 
