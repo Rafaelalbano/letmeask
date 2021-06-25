2d84a016266ebaabf3b16bbcf6cac195cf45813d
Rafael Albano <rafaelb.albano@gmail.com>
2021-06-24 21:42:33 -0300

    Comment buttons, firebase build, deploy

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -10,6 +10,8 @@ import { useRoom } from '../hooks/useRoom';
 import '../styles/room.scss';
 import deleteImg from '../assets/images/delete.svg';
 import { database } from '../services/firebase';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 
 
 
@@ -41,6 +43,18 @@ export function AdminRoom(){
     }
   }
 
+  async function handleCheckQuestionAsAnswered(questionId: string){
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighligthQuestion(questionId: string){
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
+  }
+
   return (
     <div id="page-room">
       <header>
@@ -64,7 +78,25 @@ export function AdminRoom(){
               key={question.id}
               content={question.content}
               author={question.author}
+              isAnswered={question.isAnswered}
+              isHighlighted={question.isHighlighted}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                    type="button"
+                    onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    </button>
+                    <button
+                    type="button"
+                    onClick={() => handleHighligthQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque à pergunta" />
+                    </button>
+                  </>
+                )}
                 <button
                 type="button"
                 onClick={() => handleDeleteQuestion(question.id)}
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -103,8 +103,11 @@ export function Room(){
               key={question.id}
               content={question.content}
               author={question.author}
+              isAnswered={question.isAnswered}
+              isHighlighted={question.isHighlighted}
               >
-                <button
+                {!question.isAnswered && (
+                  <button
                   className={`like-button ${question.likeId ? 'liked' : ''}`}
                   type="button"
                   aria-label="Marcar como gostei"
@@ -116,6 +119,7 @@ export function Room(){
                   </svg>
 
                 </button> 
+                )}
               </Question>
             );
           })}
